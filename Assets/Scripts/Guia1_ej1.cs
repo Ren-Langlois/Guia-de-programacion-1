1f31d515da31d881b3a50a544a77da2d9574ecf9
Ren-Langlois <101406518+Ren-Langlois@users.noreply.github.com>
2022-03-28 11:46:39 -0300

    exercise 1 a 5
    ej 1 al 5

---
+++ Assets/Scripts/Guia1_ej1.cs
@@ -0,0 +1,32 @@
+﻿
+
+//Realizá un programa donde se cree una variable entera llamada num1, que inicialmente  valdrá 0.
+//Luego incrementá su valor en 2 y mostralo por pantalla.
+//Después mostrá el  resultado de multiplicarlo por sí mismo. 
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guia1_ej1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int num1 = 0;
+
+        num1 += 2;
+
+        Debug.Log(num1);
+
+        num1 = num1 * num1;
+
+        Debug.Log(num1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Guia1_ej2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guia1_ej2 : MonoBehaviour
+{
+    //2. Realizá un programa que, siendo num1 y num2 variables enteras con valores 4 y 5,  
+    //  respectivamente, realice la operación correspondiente y muestre el resultado en pantalla: 
+    //  a.Suma
+    //  b.Producto
+
+    // Start is called before the first frame update
+
+    public int num1;
+    public int num2;
+    void Start()
+    {
+        num1 = 4;
+        num2 = 5;
+
+        int ressum = num1 + num2;
+        Debug.Log(ressum);
+        int respro = num1 * num2;
+        Debug.Log(respro);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
