fabfcf3fb347f4f9a8cbc93232e2341614abd396
Ren-Langlois <101406518+Ren-Langlois@users.noreply.github.com>
2022-04-04 10:38:35 -0300

    exercise 9 and 10

--- Assets/Scripts/Guia1_ej9.cs
+++ Assets/Scripts/Guia1_ej9.cs
@@ -13,10 +13,27 @@ using UnityEngine;
 public class Guia1_ej9 : MonoBehaviour
 {
     // Start is called before the first frame update
+    public string pers1;
+    public int cap1;
+    public string pers2;
+    public int cap2;
+    public string pers3;
+    public int cap3;
+
     void Start()
     {
-        
+        int captotal = cap1 + cap2 + cap3;
+        int por1 = cap1 * 100 / captotal ;
+        int por2 = cap2 * 100 / captotal ;
+        int por3 = cap3 * 100 / captotal ;
+
+       
+        Debug.Log("Nombre: " + pers1 + ", capital aportado: $" + cap1 + ", Porcentaje del capital: %" + por1 + ", Monto total aportado: $" + captotal);
+        Debug.Log("Nombre: " + pers2 + ", capital aportado: $" + cap2 + ", Porcentaje del capital: %" + por2 + ", Monto total aportado: $" + captotal);
+        Debug.Log("Nombre: " + pers3 + ", capital aportado: $" + cap3 + ", Porcentaje del capital: %" + por3 + ", Monto total aportado: $" + captotal);
     }
+
+    
 
     // Update is called once per frame
     void Update()
